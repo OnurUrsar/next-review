c464157e6fcca680d1696fd00de84ad49a3ae07d
ursanor <89756520+OnurUrsar@users.noreply.github.com>
2024-04-22 23:47:33 +0300

    tailwind, css, some style in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import Link from "next/link";
+import "./globals.css";
 
 interface LayoutProps {
   children: ReactNode;
@@ -8,10 +9,10 @@ interface LayoutProps {
 function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex flex-col min-h-screen px-4 py-2">
         <header>
           <nav>
-            <ul>
+            <ul className="flex gap-2">
               <li>
                 <Link href="/">Home</Link>
               </li>
@@ -26,8 +27,8 @@ function RootLayout({ children }: LayoutProps) {
             </ul>
           </nav>
         </header>
-        <main>{children}</main>
-        <footer>
+        <main className="py-3 grow">{children}</main>
+        <footer className="text-center text-xs border-t py-3">
           Game data and images courtesy of{" "}
           <a href="https://rawg.io/" target="_blank">
             RAWG
